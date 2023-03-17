93b8dbb224ebb052f1ac8a34372a6f5812b4b446
Marcelo689 <marcelosj7159@gmail.com>
2023-03-17 19:22:45 -0300

    filling month correctly

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 // document.addEventListener("onload",function (){
 //     fillMonthDays(28);
 // })
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
 
-function fillMonthDays(numDays, totalRows, dayWeekStartMonth){
+  function fillMonthDays(totalRows, dayWeekStartMonth){
+    
+    const lastDay = getDaysInMonth(2023,3);
+    numDays = lastDay;
     var table = document.getElementsByTagName("tbody")[0];
     var counter =1;
     var firstRowFilled= false;
+    var firstRowdiv = document.createElement("div");
+    firstRowdiv.className = "weekdays";
+
     for(let numRows = 1; numRows <= totalRows; numRows++){
         
         var tr = document.createElement("tr");
         for(let dayNumber =1; dayNumber <= 7; dayNumber++){
-            if(!firstRowFilled){
-                tr.className = "weekdays";
-                firstRowFilled = true;
-                dayNumber = dayWeekStartMonth;
-            }
-            var td = document.createElement("td");
-            td.textContent = counter;
             
-            console.log(dayNumber);
-            tr.appendChild(td);
-            counter++;
+            var td = document.createElement("td");
+            if(dayNumber <= dayWeekStartMonth && !firstRowFilled ){
+                for(let emptyCell = 1; emptyCell <= dayWeekStartMonth; emptyCell++){
+                    var td = document.createElement("td");
+                    td.textContent = "   ";   
+                    tr.appendChild(td);
+                }
+                dayNumber =dayWeekStartMonth;
+            }
+            
+            if(!firstRowFilled){
+                firstRowFilled = true;
+                tr.className = "weekdays";
+            }
+            
+            td.textContent = counter;
+            if(counter <= numDays){
+                tr.appendChild(td);
+                counter++;
+            }
+            
         }
+        
         table.appendChild(tr);
         if(numDays == counter)
             return;
@@ -29,4 +50,4 @@ function fillMonthDays(numDays, totalRows, dayWeekStartMonth){
 
 }
 
-fillMonthDays(28, 5, 4);
+fillMonthDays(5, 4);
